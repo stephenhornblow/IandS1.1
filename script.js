e539c1c716255704e482c175f2e776f285810538
stephenhornblow <stephenhornblow@hotmail.com>
2025-05-30 21:18:30 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,469 @@
+const cds = [
+    {
+      title: "Circles",
+      artist: "Mac Miller",
+      year: "2020",
+      cover: "covers/CirclesCover.png",
+      comment: "An actually good posthumous album.",
+      rating: "8/10",
+      genre: "Hip Hop, Funk",
+      favoriteTrack: "Woods",
+      spotifyId: "5sY6UIQ32GqwMLAfSNEaXb",
+      
+    },
+    {
+      title: "All I Have",
+      artist: "Amerie",
+      year: "2002",
+      cover: "covers/AllIHaveCover.png",
+      comment: "One of my favorite R&B albums of all time.",
+      rating: "8/10",
+      genre: "R&B",
+      favoriteTrack: "Float",
+      spotifyId: "3KUaoEXmzVovDKWionaofI",
+    },
+    {
+      title: "CONGLOMERATE (Signed)",
+      artist: "Lancey Foux, Fimiguerrero & Len",
+      year: "2024",
+      cover: "covers/ConglomerateCover.png",
+      comment: "One of the best representations of the UK scene right now!!!",
+      rating: "6.5/10",
+      genre: "Trap",
+      favoriteTrack: "Ozempic",
+      spotifyId: "3cb1xcLTVs1ovpTCXAq2EP",
+    },
+    {
+        title: "Speakerboxxx/The Love Below",
+        artist: "Outkast",
+        year: "2003",
+        cover: "covers/TLBCover.png",
+        comment: "Not spoken about enough outside of Hey Ya! and Roses...",
+        rating: "7/10",
+        genre: "Rap",
+        favoriteTrack: "Prototype",
+        spotifyId: "1UsmQ3bpJTyK6ygoOOjG1r",
+    },
+    {
+        title: "Ignorance Is Bliss",
+        artist: "Skepta",
+        year: "2019",
+        cover: "covers/IBCover.png",
+        comment: "Cold.",
+        rating: "9/10",
+        genre: "Grime",
+        favoriteTrack: "What Do You Mean? (ft. J Hus)",
+        spotifyId: "22zpCX6Nb9ppOVklalvGec",
+    },
+    {
+        title: "2014 Forest Hills Drive (Alt Cover)",
+        artist: "J. Cole",
+        year: "2024 (Original Album produced in 2014)",
+        cover: "covers/2014FHDCover.png",
+        comment: "Gatekeeping the bonus tracks by the way.",
+        rating: "8.5/10",
+        genre: "Rap",
+        favoriteTrack: "'03 Adolescence",
+        spotifyId: "0UMMIkurRUmkruZ3KGBLtG",
+    },
+    {
+        title: "Swimming",
+        artist: "Mac Miller",
+        year: "2018",
+        cover: "covers/SwimmingCover.png",
+        comment: "This album got me through some of the toughest periods of my life.",
+        rating: "8/10",
+        genre: "Hip Hop, Jazz Rap",
+        favoriteTrack: "2009",
+        spotifyId: "5wtE5aLX5r7jOosmPhJhhk",
+    },
+    {
+        title: "self titled",
+        artist: "Destiny's Child",
+        year: "1998",
+        cover: "FillerImage.webp",
+        comment: "Quintissential 90's R&B",
+        rating: "7/10",
+        genre: "Neo Soul, R&B",
+        favoriteTrack: "Bridges",
+        spotifyId: "7D7VhHKEee3UMA0PtbMEMQ",
+    },
+    {
+        title: "Songs About Jane",
+        artist: "Maroon 5",
+        year: "2002",
+        cover: "FillerImage.webp",
+        comment: "Maroon 5 kinda fell off after this. This is tresure.",
+        rating: "7.5/10",
+        genre: "Funk Rock, Pop Rock",
+        favoriteTrack: "Sunday Morning",
+        spotifyId: "1Rv9WRKyYhFaGbuYDaQunN",
+    },
+    {
+        title: "Late Registration",
+        artist: "Kanye West",
+        year: "2005",
+        cover: "covers/LateRegistrationCover.png",
+        comment: "Can I talk my shit again?",
+        rating: "8/10",
+        genre: "Hip Hop",
+        favoriteTrack: "Addiction",
+        spotifyId: "0Ds6i3h0F9RcYIKAD5Olum",
+    },
+    {
+        title: "Songs",
+        artist: "Luther Vandross",
+        year: "1994",
+        cover: "covers/SongsCover.png",
+        comment: "Grammy nominated for a reason.",
+        rating: "6/10",
+        genre: "R&B",
+        favoriteTrack: "Since You've Been Gone",
+        spotifyId: "79RyzfidCUY1XvdzwW0AYy",
+    },
+    {
+        title: "The Diary Of Alicia Keys",
+        artist: "Alicia Keys",
+        year: "2003",
+        cover: "covers/TDAKCover.png",
+        comment: "Masterpiece.",
+        rating: "9/10",
+        genre: "R&B, Soul",
+        favoriteTrack: "Samsonite Man",
+        spotifyId: "6TqRKHLjDu5QZuC8u5Woij",
+        
+    }, 
+    {
+        title: "Loose",
+        artist: "Nelly Furtado",
+        year: "2006",
+        cover: "covers/LooseCover.png",
+        comment: "Nelly Furtado and Timbaland were a match made in heaven.",
+        rating: "7.5/10",
+        genre: "Pop",
+        favoriteTrack: "Do It",
+        spotifyId: "2yboV2QBcVGEhcRlYuPpDT",
+        
+    },
+    {
+        title: "The Score",
+        artist: "Fugees",
+        year: "1996",
+        cover: "covers/TheScoreCover.png",
+        comment: "My dad was going through it when they split up in '98.",
+        rating: "7/10",
+        genre: "Alternative Hip Hop",
+        favoriteTrack: "Zealots",
+        spotifyId: "4z6F5s3RVaOsekuaegbLfD",
+    },
+    {
+        title: "self titled",
+        artist: "Bobby V.",
+        year: "2005",
+        cover: "covers/BobbyValentinoCover.png",
+        comment: "Great but nothing new.",
+        rating: "5/10",
+        genre: "R&B",
+        favoriteTrack: "Give Me A Chance (with Ludacris)",
+        spotifyId: "6tiqcLj9TP3qaxWUbNuPYn",
+        
+    },
+    {
+        title: "Take Off Your Pants And Jacket",
+        artist: "Blink-182",
+        year: "2001",
+        cover: "covers/B182Cover.png",
+        comment: "I like this album.",
+        rating: "6.5/10",
+        genre: "Pop-punk, Punk Rock",
+        favoriteTrack: "Roller Coaster",
+        spotifyId: "3nHpBmW5wJXGeC3ojBkpey"
+    },
+    {
+        title: "self titled",
+        artist: "Beyoncé",
+        year: "2013",
+        cover: "covers/BeyCover.png",
+        comment: "Who doesn't like Bey.",
+        rating: "7/10",
+        genre: "Alternative R&B",
+        favoriteTrack: "Superpower (ft. Frank Ocean",
+        spotifyId: "2UJwKSBUz6rtW4QLK74kQu",
+        
+    },
+    {
+        title: "Thriller",
+        artist: "Michael Jackson",
+        year: "1982",
+        cover: "FillerImage.webp",
+        comment: "Classic. Can't say too much more.",
+        rating: "8.5/10",
+        genre: "Pop",
+        favoriteTrack: "P.Y.T. (Pretty Young Thing)",
+        spotifyId: "2ANVost0y2y52ema1E9xAZ",
+    },
+    {
+        title: "Parachutes",
+        artist: "Coldplay",
+        year: "2000",
+        cover: "covers/ParachutesCover.png",
+        comment: "Pretty Solid.",
+        rating: "5.5/10",
+        genre: "Alternative Rock",
+        favoriteTrack: "We Never Change",
+        spotifyId: "6ZG5lRT77aJ3btmArcykra",
+        
+    },
+    {
+        title: "The College Dropout",
+        artist: "Kanye West",
+        year: "2004",
+        cover: "covers/TCDCover.png",
+        comment: "So many classics on here.",
+        rating: "7.5/10",
+        genre: "Hip Hop",
+        favoriteTrack: "Through The Wire",
+        spotifyId: "4Uv86qWpGTxf7fU7lG5X6F",
+    },
+    {
+        title: "Confessions",
+        artist: "Usher",
+        year: "2004",
+        cover: "covers/ConfessionsCover.png",
+        comment: "Highkey one of the most important R&B records ever.",
+        rating: "7/10",
+        genre: "R&B",
+        favoriteTrack: "Burn",
+        spotifyId: "1RM6MGv6bcl6NrAG8PGoZk",
+    },
+    {
+        title: "All For You",
+        artist: "Janet Jackson",
+        year: "2001",
+        cover: "covers/AllForYouCover.png",
+        comment: "Inspired so many other artists that blew up around the early 2000's/2010's.",
+        rating: "6/10",
+        genre: "Pop, R&B",
+        favoriteTrack: "Feels So Right",
+        spotifyId: "0IfJey7ibEDtWNxAQw2ub9",
+    },
+    {
+        title: "A Girl Like Me",
+        artist: "Rihanna",
+        year: "2006",
+        cover: "covers/AGirlLikeMeCover.png",
+        comment: "Overshadowed by the rest of her career but still insanely good.",
+        rating: "7.5/10",
+        genre: "Pop, Reggae",
+        favoriteTrack: "We Ride",
+        spotifyId: "2JdjS6jjOml7nt7Yjo0nnh",
+    },
+    {
+        title: "Fancy That",
+        artist: "PinkPantheress",
+        year: "2025",
+        cover: "covers/FancyThatCover.png",
+        comment: "Pink took a new direction and it's growing on me.",
+        rating: "6/10",
+        genre: "Dance-pop, UK Garage",
+        favoriteTrack: "Noises",
+        spotifyId: "3o81FH5FTYVtoBQOZr3T7y",
+       
+    },
+    {
+        title: "Idlewild",
+        artist: "Outkast",
+        year: "2006",
+        cover: "covers/IdlewildCover.png",
+        comment: "Underrated, but definitely not their best.",
+        rating: "5/10",
+        genre: "Hip Hop",
+        favoriteTrack: "The Train (ft. Scar & Sleepy Brown)",
+        spotifyId: "3L6JBfRxhzGzhhRDlTEdBz",
+    },
+    {
+        title: "My Way",
+        artist: "Usher",
+        year: "1997",
+        cover: "covers/MyWayCover.png",
+        comment: "Usher's breakthrough album.",
+        rating: "5/10",
+        genre: "R&B",
+        favoriteTrack: "You Make Me Wanna...",
+        spotifyId: "0fQdoem8dnrl80YcZzQ8f0",
+        
+    }
+
+
+  ];
+  
+  const visibleCount = 5;
+const track = document.getElementById("carousel-track");
+let currentIndex = 0;
+let carouselItems = [];        
+let allCarouselItems = [];     
+
+function createCarousel() {
+  cds.forEach((cd, index) => {
+    const item = createCarouselItem(cd, index);
+    track.appendChild(item);
+    carouselItems.push(item);
+    allCarouselItems.push(item);
+  });
+
+  
+  for (let i = cds.length - visibleCount; i < cds.length; i++) {
+    const clone = createCarouselItem(cds[i], i);
+    clone.classList.add("clone");
+    track.insertBefore(clone, track.firstChild);
+    allCarouselItems.unshift(clone);
+  }
+
+  
+  for (let i = 0; i < visibleCount; i++) {
+    const clone = createCarouselItem(cds[i], i);
+    clone.classList.add("clone");
+    track.appendChild(clone);
+    allCarouselItems.push(clone);
+  }
+}
+
+function createCarouselItem(cd, index) {
+  const item = document.createElement("img");
+  item.src = cd.cover;
+  item.className = "carousel-item";
+  item.dataset.index = index;
+  item.addEventListener("click", () => selectCD(index));
+  return item;
+}
+
+function selectCD(index) {
+  currentIndex = index + visibleCount; 
+  updateDisplay();
+  updateCarousel();
+}
+
+function updateDisplay() {
+    const cd = cds[currentIndex - visibleCount]; 
+  
+    
+    document.getElementById("album-title").textContent = cd.title;
+    document.getElementById("album-artist").textContent = cd.artist;
+    document.getElementById("album-year").textContent = cd.year;
+    document.getElementById("user-comment").textContent = cd.comment;
+    document.getElementById("rating").textContent = cd.rating;
+    document.getElementById("genre").textContent = cd.genre;
+    document.getElementById("favorite-track").textContent = cd.favoriteTrack;
+  
+    // Show ONLY the disk in the main display (use cover only as fallback)
+    const cdCover = document.getElementById("cd-cover");
+    cdCover.innerHTML = ''; // Clear previous image
+  
+    const image = document.createElement("img");
+    if (cd.disk) {
+      image.src = cd.disk;
+      image.alt = "CD Disk";
+      image.classList.add("cd-disk");
+    } else if (cd.cover) {
+      image.src = cd.cover;
+      image.alt = "CD Cover";
+      image.classList.add("cd-image");
+    } else {
+      image.alt = "No image available";
+    }
+    cdCover.appendChild(image);
+  
+    // Spotify Embed
+    const player = document.getElementById("spotify-player");
+    if (cd.spotifyId) {
+      player.innerHTML = `
+        <iframe style="border-radius:12px"
+          src="https://open.spotify.com/embed/album/${cd.spotifyId}?utm_source=generator&theme=0"
+          width="100%" height="152" frameBorder="0" allowfullscreen=""
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+          loading="lazy">
+        </iframe>`;
+    } else {
+      player.innerHTML = "<p><em>No Spotify link available</em></p>";
+    }
+  
+    
+    document.querySelectorAll('.info-pane').forEach(pane => {
+      pane.classList.remove('active');
+      void pane.offsetWidth; 
+      pane.classList.add('active');
+    });
+  }
+  
+  
+
+function updateCarousel() {
+  allCarouselItems.forEach(item => item.classList.remove("selected"));
+
+  const selectedItem = allCarouselItems[currentIndex];
+  if (!selectedItem) return;
+
+  selectedItem.classList.add("selected");
+  centerItem(selectedItem);
+
+  const total = cds.length;
+
+  setTimeout(() => {
+    if (currentIndex < visibleCount) {
+      
+      currentIndex += total;
+      const realItem = allCarouselItems[currentIndex];
+      jumpToIndex(realItem);
+    }
+
+    if (currentIndex >= total + visibleCount) {
+      
+      currentIndex -= total;
+      const realItem = allCarouselItems[currentIndex];
+      jumpToIndex(realItem);
+    }
+  }, 500);
+}
+
+function centerItem(item) {
+  const wrapper = document.getElementById("carousel-wrapper");
+  const wrapperCenter = wrapper.offsetWidth / 2;
+  const itemRect = item.getBoundingClientRect();
+  const wrapperRect = wrapper.getBoundingClientRect();
+  const itemCenter = itemRect.left + itemRect.width / 2;
+
+  const delta = wrapperCenter - itemCenter;
+
+  const currentTransform = track.style.transform.match(/-?\d+\.?\d*/g);
+  const currentTranslateX = currentTransform ? parseFloat(currentTransform[0]) : 0;
+  const newTranslateX = currentTranslateX + delta;
+
+  track.style.transition = "transform 0.5s ease";
+  track.style.transform = `translateX(${newTranslateX}px)`;
+}
+
+function jumpToIndex(item) {
+  const wrapper = document.getElementById("carousel-wrapper");
+  const wrapperCenter = wrapper.offsetWidth / 2;
+  const itemRect = item.getBoundingClientRect();
+  const itemCenter = itemRect.left + itemRect.width / 2;
+
+  const delta = wrapperCenter - itemCenter;
+
+  requestAnimationFrame(() => {
+    track.style.transition = "none";
+    const currentTransform = track.style.transform.match(/-?\d+\.?\d*/g);
+    const currentTranslateX = currentTransform ? parseFloat(currentTransform[0]) : 0;
+    const newTranslateX = currentTranslateX + delta;
+    track.style.transform = `translateX(${newTranslateX}px)`;
+
+    void track.offsetWidth; 
+    track.style.transition = "transform 0.5s ease";
+    allCarouselItems.forEach(i => i.classList.remove("selected"));
+    item.classList.add("selected");
+  });
+}
+
+
+createCarousel();
+selectCD(0);
